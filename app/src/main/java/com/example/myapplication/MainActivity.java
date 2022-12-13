3a74600374dff69f33369a78fa30d69218ad8216
stvitanh <vinsky.s.tanhansen@stud.h-da.de>
2022-12-13 21:50:17 +0100

    new Function

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,58 +1,90 @@
 package com.example.myapplication;
 
 import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.app.ActivityManager;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Switch;
 import android.widget.CompoundButton;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
+
+import com.example.myapplication.databinding.ActivityMainBinding;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
-    Button A;
-    TextView B;
-    Button A2;
-    Switch darkmode;
+    Button Arzt;
+    Button Verwalter;
+    Button Labor;
+    BottomNavigationView bottomNavigationView;
     @SuppressLint({"SetTextI18n", "MissingInflatedId"})
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        B=findViewById(R.id.Username);
-        A=findViewById(R.id.Login);
-        darkmode=findViewById(R.id.Darkmode);
-        Intent intent = new Intent(this, MainActivity2.class);
-        A.setOnClickListener(new View.OnClickListener() {
+        Arzt=findViewById(R.id.Arzt);
+        Verwalter=findViewById(R.id.Verwalter);
+        Labor=findViewById(R.id.Labor);
+        Intent intent = new Intent(this, ArztSearchActivity.class);
+        Intent intent2 = new Intent(this, MainActivity2.class);
+        Intent intent3 = new Intent(this, LaborMenu.class);
+        Arzt.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 startActivity(intent);
             }
         });
-        darkmode.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+
+        Verwalter.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
-                if(b){
-                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
-                    darkmode.setText("Dark mode");
-                }else{
-                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
-                    darkmode.setText("Light mode");
-                }
+            public void onClick(View view) {
+                startActivity(intent2);
             }
         });
-        //set night mode when app starts?
-        boolean isNightModeOn = AppCompatDelegate.getDefaultNightMode() == AppCompatDelegate.MODE_NIGHT_YES;
-            darkmode.setChecked(isNightModeOn);
-            if(isNightModeOn){
-                darkmode.setText("Night Mode");
-            }else{
-                darkmode.setText("Light Mode");
+
+        Labor.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(intent3);
             }
+        });
+        //BottomNavigationBar
+        bottomNavigationView = findViewById(R.id.bottomnavigationbar);
+        bottomNavigationView.setSelectedItemId(R.id.home);
+        //implementation of BottomNavigationBar
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.home:
+                        return true;
+                    case R.id.darkmode:
+                        startActivity(new Intent(getApplicationContext()
+                                ,DarkModeActivity.class));
+                        overridePendingTransition(0,0);
+                        return true;
+                    case R.id.info:
+                        startActivity(new Intent(getApplicationContext()
+                                ,Info.class));
+                        overridePendingTransition(0,0);
+                        return true;
+                }
+                return false;
+            }
+        });
+
     }
+
+
 }
--- app/src/main/java/com/example/myapplication/MainActivity2.java
+++ app/src/main/java/com/example/myapplication/MainActivity2.java
@@ -2,17 +2,23 @@ package com.example.myapplication;
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 public class MainActivity2 extends AppCompatActivity {
     Button plus;
+    BottomNavigationView bottomNavigationView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,6 +29,35 @@ public class MainActivity2 extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 startActivity(addpatient);
+            }
+        });
+
+
+
+        //BottomNavigationBar
+        bottomNavigationView = findViewById(R.id.bottomnavigationbar);
+        bottomNavigationView.setSelectedItemId(R.id.home);
+        //implementation of BottomNavigationBar
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.home:
+                        startActivity(new Intent(getApplicationContext()
+                                ,MainActivity.class));
+                        return true;
+                    case R.id.darkmode:
+                        startActivity(new Intent(getApplicationContext()
+                                ,DarkModeActivity.class));
+                        overridePendingTransition(0,0);
+                        return true;
+                    case R.id.info:
+                        startActivity(new Intent(getApplicationContext()
+                                ,Info.class));
+                        overridePendingTransition(0,0);
+                        return true;
+                }
+                return false;
             }
         });
     }
