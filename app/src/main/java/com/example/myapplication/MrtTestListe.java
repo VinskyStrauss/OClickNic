0345e1d041fb0e32ee8cc53466b20414e6a4efb3
TanHanSen,Vinsky Strauss <vinsky.strauss@gmail.com>
2022-12-14 15:54:53 +0100

    fixed some Bugs

--- app/src/main/java/com/example/myapplication/MrtTestListe.java
+++ app/src/main/java/com/example/myapplication/MrtTestListe.java
@@ -8,22 +8,52 @@ import androidx.fragment.app.FragmentTransaction;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.view.View;
+import android.widget.ListView;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+import java.util.ArrayList;
+import java.util.List;
+
+
+
 public class MrtTestListe extends AppCompatActivity {
-    Button result;
     BottomNavigationView bottomNavigationView;
+    Fragment fragment;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mrt_test_liste);
-        result = findViewById(R.id.viewResult);
+        //Fragment
+        fragment = new MrtResultFragment();
+        //Check the result from MRT test
+        ListView listView = findViewById(R.id.testList);
+        List<String> list = new ArrayList<>();
+        list.add("Julia Bettmann");
+        list.add("Thomas Müller");
+        list.add("Elene Kasper");
+
+        ArrayAdapter arrayAdapter = new ArrayAdapter(getApplicationContext(), android.R.layout.simple_expandable_list_item_1,list);
+        listView.setAdapter(arrayAdapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+                if(position == 0){
+                    FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+                    fragmentTransaction.replace(R.id.testListeactv,fragment);
+                    fragmentTransaction.commit();
 
 
+                }
 
+            };
+
+        });
 
         //BottomNavigationBar
         bottomNavigationView = findViewById(R.id.bottomnavigationbar);
