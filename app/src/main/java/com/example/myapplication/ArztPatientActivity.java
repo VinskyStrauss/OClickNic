632b91decdfea9aaf346ce93c79a11b9bdaf19da
fabrice <fabrice@example.com>
2022-12-14 13:30:09 +0100

    activity_patient_list in activity_patient_daten.xml umgewandelt. Liste von Patienten erstellt

--- app/src/main/java/com/example/myapplication/ArztPatientActivity.java
+++ app/src/main/java/com/example/myapplication/ArztPatientActivity.java
@@ -2,7 +2,15 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ArztPatientActivity extends AppCompatActivity {
 
@@ -10,5 +18,36 @@ public class ArztPatientActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_arzt_patient);
+
+        ListView listView = findViewById(R.id.listview);
+        List<String> list = new ArrayList<>();
+        list.add("Julia Bettmann");
+        list.add("Thomas Müller");
+        list.add("Elene Kasper");
+
+        ArrayAdapter arrayAdapter = new ArrayAdapter(getApplicationContext(), android.R.layout.simple_expandable_list_item_1,list);
+        listView.setAdapter(arrayAdapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+                if(position == 0){
+                    startActivity(new Intent(ArztPatientActivity.this,PatientList.class));
+                }
+
+
+
+            };
+
+        });
+
+
+
+
+
+
+
     }
+
+
 }
