770d538feed21ad048438535c93396d79a18724f
TanHanSen,Vinsky Strauss <vinsky.strauss@gmail.com>
2022-12-14 13:28:25 +0100

    added new Function

--- app/src/main/java/com/example/myapplication/BlutTestListe.java
+++ app/src/main/java/com/example/myapplication/BlutTestListe.java
@@ -1,13 +1,45 @@
 package com.example.myapplication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class BlutTestListe extends AppCompatActivity {
+    BottomNavigationView bottomNavigationView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_blut_test_liste);
+        //BottomNavigationBar
+        bottomNavigationView = findViewById(R.id.bottomnavigationbar);
+        bottomNavigationView.setSelectedItemId(R.id.home);
+        //implementation of BottomNavigationBar
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.home:
+                        startActivity(new Intent(getApplicationContext()
+                                ,MainActivity.class));
+                        return true;
+                    case R.id.darkmode:
+                        startActivity(new Intent(getApplicationContext()
+                                ,DarkModeActivity.class));
+                        overridePendingTransition(0,0);
+                        return true;
+                    case R.id.info:
+                        startActivity(new Intent(getApplicationContext()
+                                ,Info.class));
+                        overridePendingTransition(0,0);
+                        return true;
+                }
+                return false;
+            }
+        });
     }
 }
--- app/src/main/java/com/example/myapplication/MrtTestListe.java
+++ app/src/main/java/com/example/myapplication/MrtTestListe.java
@@ -1,14 +1,55 @@
 package com.example.myapplication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.view.View;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MrtTestListe extends AppCompatActivity {
-
+    Button result;
+    BottomNavigationView bottomNavigationView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mrt_test_liste);
+        result = findViewById(R.id.viewResult);
+
+
+
+
+        //BottomNavigationBar
+        bottomNavigationView = findViewById(R.id.bottomnavigationbar);
+        bottomNavigationView.setSelectedItemId(R.id.home);
+        //implementation of BottomNavigationBar
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.home:
+                        startActivity(new Intent(getApplicationContext()
+                                ,MainActivity.class));
+                        return true;
+                    case R.id.darkmode:
+                        startActivity(new Intent(getApplicationContext()
+                                ,DarkModeActivity.class));
+                        overridePendingTransition(0,0);
+                        return true;
+                    case R.id.info:
+                        startActivity(new Intent(getApplicationContext()
+                                ,Info.class));
+                        overridePendingTransition(0,0);
+                        return true;
+                }
+                return false;
+            }
+        });
     }
 }
